0862d0ccae7c83a24baaa7f296080bb1cc28779b
Matt Shildt <matt.shildt@gmail.com>
2015-11-02 14:04:54 -0500

    Use namedtuple for points, fence now initializes coordinates.

--- hr/mrkm/mrkm.py
+++ hr/mrkm/mrkm.py
@@ -1,15 +1,28 @@
 import sys
+from collections import namedtuple
+
+Point = namedtuple('Point', ['x', 'y'])
 
 class Fence():
-    def __init__(self, width, height, origin):
-        '''origin is a two-tuple representing upper leftmost coordinate.'''
+    def __init__(self, width, height, pos):
+        '''pos is a Point representing upper leftmost coordinate.'''
         self.width = width
         self.height = height
-        self.origin = origin
-        self.coordinates = self.init_coords()
+        self.pos = pos
+        self.init_coords()
 
 
     def init_coords(self):
+        self.coordinates = set()
+
+        for row in xrange(self.height):
+            self.coordinates.add( Point(self.pos.x, self.pos.y + row) )
+            self.coordinates.add( Point(self.pos.x + self.width - 1, self.pos.y + row) )
+
+            if row == 0 or row == self.height - 1:
+                self.coordinates.update(
+                        [ Point(xcoord, self.pos.y + row) for xcoord in
+                          xrange(self.pos.x + 1, self.pos.x + self.width - 1) ])
         
 
 
@@ -17,16 +30,16 @@ def init_marsh():
     """Marsh will be represented as a set of coordinates."""
 
     marsh = set()
-    row = 0
+    y = 0
     
     for line in sys.stdin.readlines():
-        col = 0
+        x = 0
     
         for c in line:
-            if c == 'x': marsh.add( (row, col) )
-            col += 1
+            if c == 'x': marsh.add( Point(x, y) )
+            x += 1
     
-        row += 1
+        y += 1
 
     return marsh
 
@@ -34,5 +47,7 @@ def init_marsh():
 if __name__ == '__main__':
     land_dims = [ int(n) for n in sys.stdin.readline().strip().split() ]
     marsh = init_marsh()
-    
+    fence = Fence(4, 2, Point(0,0))
+
     print marsh
+    print fence.coordinates
